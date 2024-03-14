49e9ad5799736e9c05ca32898736cb42d689804b
mrothauer <office@rothauer-it.com>
2024-03-14 11:02:59 +0100

    de causes problems

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,10 +30,10 @@ const config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'de',
-    locales: ['de'],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
-
+  
   presets: [
     [
       'classic',
