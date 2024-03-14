7384b6998819881b9a781ab555cd6e1fc23b3b14
mrothauer <office@rothauer-it.com>
2024-03-14 11:11:22 +0100

    de

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,7 @@ const config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
+    defaultLocale: 'de',
     locales: ['de'],
   },
 
